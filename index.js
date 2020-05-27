8c950d4d1f800ddf130bce4fc4d1364d428e19d4
Saurabh <dashsaurabh@gmail.com>
2020-05-27 11:22:11 +0530

    Adding container id support

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const http = require("http");
+const getId = require("docker-container-id");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  let id = await getId();
   res.writeHead(200, {
     "Content-type": "text/html",
   });
-  res.end("<h1>Hello World from OpenShift NodeJS App</h1>");
+  res.end(
+    `<h1>Hello World from OpenShift NodeJS App from ContainerId#${id}</h1>`
+  );
 });
 
 server.listen(8000, () => {
