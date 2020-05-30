c5a15be4cd0ff5389c3b35865173d4d54bd7d949
Saurabh <dashsaurabh@gmail.com>
2020-05-30 15:43:52 +0530

    C

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer(async (req, res) => {
-  res.writeHead(200, {
-    "Content-type": "text/html",
-  });
-  res.end(
-    `<h1>Hello World from OpenShift NodeJS App in POD NAME: ${process.env.MY_POD_NAME}</h1>`
-  );
+  const { pathname } = url.parse(req.url);
+
+  if (pathname === "/health") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    res.end("<h1>Application is Up and Running</h1>");
+  } else if (pathname === "/unhealthy") {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+
+    res.end("<h1>Page Not Found</h1>");
+  } else {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(
+      `<h1>Hello World from OpenShift NodeJS App in POD NAME: ${process.env.MY_POD_NAME}</h1>`
+    );
+  }
 });
 
 server.listen(8000, () => {
