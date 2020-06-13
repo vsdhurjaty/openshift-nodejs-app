8f3cf9311da1e6f7f9153a8513856ae07a968ad9
Saurabh <dashsaurabh@gmail.com>
2020-06-13 10:55:43 +0530

    Commit

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-  });
+app.get("/", (req, res) => {
+  const html = `<h1>Hello World from our OpenShift NodeJS App</h1>
+  <p>My Pod Name: ${process.env.MY_POD_NAME}</p>
+  <p>My Application Name: ${process.env.MY_APPLICATION_NAME}</p>`;
 
-  const html = `<h1>Hello World from OpenShift NodeJS Application</h1>
-  <p>Pod Name: ${process.env.MY_POD_NAME}</p>
-  <p>Application Name: ${process.env.MY_APPLICATION_NAME}</p>`;
-
-  res.end(html);
+  res.status(200).send(html);
 });
 
-server.listen(8000, () => {
-  console.log("Listening to requests on Port 8000");
+app.get("/health", (req, res) => {
+  const html = "<h1>Application is Healthy</h1>";
+
+  res.status(200).send(html);
+});
+
+app.get("/unhealthy", (req, res) => {
+  const html = "<h1>Application is Unhealthy</h1>";
+  res.status(404).send(html);
+});
+
+app.listen(8000, () => {
+  console.log("Application is listening to requests on port 8000");
 });
